063a83f16044c6dfc66925027e64703024637c8a
flffkd12 <flffkd12@gmail.com>
2025-12-12 20:30:35 +0900

    refactor: 초기 데이터 생성 관련 관심사 분리

--- src/main/java/com/devmatch/backend/global/init/BaseInitData.java
+++ src/main/java/com/devmatch/backend/global/init/BaseInitData.java
@@ -1,50 +1,22 @@
 package com.devmatch.backend.global.init;
 
-import com.devmatch.backend.domain.user.entity.User;
-import com.devmatch.backend.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 @RequiredArgsConstructor
 public class BaseInitData {
 
-  @Autowired
-  @Lazy
-  private BaseInitData self;
-
-  private final UserService userService;
+  private final InitialDataService initialDataService;
 
   @Bean
   ApplicationRunner baseInitDataApplicationRunner() {
     return args -> {
-      self.work1();
+      initialDataService.createInitialUsers();
     };
-  }
-
-  @Transactional
-  public void work1() {
-    if (userService.count() > 0) {
-      return;
-    }
-    User memberSystem = userService.join("system", "시스템");
-    memberSystem.modifyApiKey(memberSystem.getUsername());
-
-    User memberAdmin = userService.join("admin", "관리자");
-    memberAdmin.modifyApiKey(memberAdmin.getUsername());
-
-    User memberUser1 = userService.join("user1", "유저1");
-    memberUser1.modifyApiKey(memberUser1.getUsername());
-
-    User memberUser2 = userService.join("user2", "유저2");
-    memberUser2.modifyApiKey(memberUser2.getUsername());
-
-    User memberUser3 = userService.join("user3", "유저3");
-    memberUser3.modifyApiKey(memberUser3.getUsername());
   }
 }
