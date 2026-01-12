bb6f82f98a042a1a8f8d1defab370f41c611ac02
flffkd12 <flffkd12@gmail.com>
2026-01-12 11:36:39 +0900

    feat: Inner Join으로 페이징 조회 시 발생하는 User 관련 N+1 문제 개선

--- src/main/java/com/devmatch/backend/domain/project/repository/ProjectRepository.java
+++ src/main/java/com/devmatch/backend/domain/project/repository/ProjectRepository.java
@@ -4,8 +4,17 @@ import com.devmatch.backend.domain.project.entity.Project;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
-  Page<Project> findAllByCreatorId(Long creatorId, Pageable pageable);
+  @Override
+  @NonNull
+  @Query("SELECT p FROM Project p JOIN FETCH p.creator")
+  Page<Project> findAll(@NonNull Pageable pageable);
+
+  @Query("SELECT p FROM Project p JOIN FETCH p.creator WHERE p.creator.id = :creatorId")
+  Page<Project> findAllByCreatorId(@Param("creatorId") Long creatorId, Pageable pageable);
 }
