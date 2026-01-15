83bcda36088253e63a86e96efcbc48180c8413ed
flffkd12 <flffkd12@gmail.com>
2026-01-15 11:38:15 +0900

    feat: 지원서 도메인에서 N:1 관련 N+1문제 fetch join 기능 추가

--- src/main/java/com/devmatch/backend/domain/application/repository/ApplicationRepository.java
+++ src/main/java/com/devmatch/backend/domain/application/repository/ApplicationRepository.java
@@ -4,15 +4,21 @@ import com.devmatch.backend.domain.application.entity.Application;
 import com.devmatch.backend.domain.application.enums.ApplicationStatus;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
 
-  List<Application> findAllByApplicantId(Long id);
+  @Query("SELECT a FROM Application a JOIN FETCH a.applicant WHERE a.applicant.id = :applicantId")
+  List<Application> findAllByApplicantId(@Param("applicantId") Long applicantId);
 
-  List<Application> findAllByProjectId(Long id);
+  @Query("SELECT a FROM Application a JOIN FETCH a.applicant WHERE a.project.id = :applicantId")
+  List<Application> findAllByProjectId(@Param("applicantId") Long applicantId);
 
   boolean existsByApplicantIdAndProjectId(Long applicantId, Long projectId);
 
-  List<Application> findByProjectIdAndStatus(Long projectId, ApplicationStatus status);
+  @Query("SELECT a FROM Application a JOIN FETCH a.applicant WHERE a.project.id = :projectId AND a.status = :status")
+  List<Application> findByProjectIdAndStatus(@Param("projectId") Long projectId,
+      @Param("status") ApplicationStatus status);
 
 }
